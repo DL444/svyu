5eafd618a73e7a3d948c9bb1f85e216e68023543
David Lee <DL444@outlook.com>
2020-03-14 00:51:39 +0800

    Implement survey serving endpoint.

---
+++ server/SvyU.Backend/SurveyEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace SvyU.Backend
+{
+    public class SurveyEntity
+    {
+        public string SurveyId { get; set; }
+
+        public string JsonPayload { get; set; }
+
+        public string PartitionKey => SurveyId;
+
+        [JsonProperty("id")]
+        public string Id => $"survey-{PartitionKey}";
+    }
+}
