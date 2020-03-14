fc9ed58c7437b00ad79f40e94382c09ce1c9e29b
David Lee <DL444@outlook.com>
2020-03-14 15:50:04 +0800

    Implement QR code generation.

--- server/SvyU.Web/Startup.cs
+++ server/SvyU.Web/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using Microsoft.AspNetCore.Components.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using SvyU.Web.Services;
+using ZXing;
 
 namespace SvyU.Web
 {
@@ -11,6 +12,14 @@ namespace SvyU.Web
         {
             services.AddSingleton(new Random((int)DateTimeOffset.UtcNow.Ticks));
             services.AddTransient<SurveyService>();
+            services.AddTransient(_ => new BarcodeWriterSvg() 
+            { 
+                Format = BarcodeFormat.QR_CODE,
+                Options = new ZXing.Common.EncodingOptions()
+                {
+                    Margin = 1
+                }
+            });
         }
 
         public void Configure(IComponentsApplicationBuilder app)
