7a68952c7b94fdee123200cc0679a329fb56648c
David Lee <DL444@outlook.com>
2020-03-18 13:29:04 +0800

    Implement response model and deserialization.

---
+++ server/SvyU.Models/MultipleResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace SvyU.Models
+{
+    public class MultipleResponse : IResponseItem
+    {
+        public QuestionType Type => QuestionType.Multiple;
+
+        [JsonProperty("answer")]
+        public int[] Response { get; set; }
+    }
+}
